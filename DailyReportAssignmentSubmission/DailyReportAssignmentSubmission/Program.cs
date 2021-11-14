89708b5fdc66d0b44e20928deb73a76dff9e1c10
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2021-11-14 17:41:37 -0500

    C#

---
+++ DailyReportAssignmentSubmission/DailyReportAssignmentSubmission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportAssignmentSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you in?");
+            Console.WriteLine("What page number?");
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.WriteLine("How many hours did you study today?");
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
+    
