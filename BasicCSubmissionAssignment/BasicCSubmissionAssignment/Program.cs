f26f897d284cbc3f7d468ce9c5a0547cfdf7ea2e
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2021-11-14 17:15:39 -0500

    C#

---
+++ BasicCSubmissionAssignment/BasicCSubmissionAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+  {
+      static void Main()
+      {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+      }
+ }
+
