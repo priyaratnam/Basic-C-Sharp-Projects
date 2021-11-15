8fea37973df2ab4a3f28a39e23a3146451058db0
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2021-11-15 08:55:54 -0500

    C#

--- DailyReportAssignmentSubmission/DailyReportAssignmentSubmission/Program.cs
+++ DailyReportAssignmentSubmission/DailyReportAssignmentSubmission/Program.cs
@@ -12,15 +12,43 @@ namespace DailyReportAssignmentSubmission
         {
             Console.WriteLine("Academy of Learning Career College");
             Console.WriteLine("Student Daily Report");
-            Console.WriteLine("What course are you in?");
-            Console.WriteLine("What page number?");
-            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            Console.WriteLine("How many hours did you study today?");
-            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
-            Console.ReadLine();
-        }
+            {
+                Console.WriteLine("What course are you in?");
+                string course = Console.ReadLine();
+                Console.WriteLine("Your course is: " + course);
+            }
+            {
+                Console.WriteLine("What page number?");
+                int page = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The page number is: " + page);
+            }
+            {
+                Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+                int help = Convert.ToInt32(Console.ReadLine());
+                bool b = false;
+                string res = b.ToString();
+                Console.WriteLine("Return Value = " + res);
+            }
+            {
+                Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+                int any = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The positive experiences are: " + any);
+            }
+            {
+                Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+                int feedback = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The feedback is: " + feedback);
+            }
+            {
+                Console.WriteLine("How many hours did you study today?");
+                int hour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The total hours is: " + hour);
+            }
+           
+                Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+                Console.ReadLine();
+          
+         }
     }
 }
     
