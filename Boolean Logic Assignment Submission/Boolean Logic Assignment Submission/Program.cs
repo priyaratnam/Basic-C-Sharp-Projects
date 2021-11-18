8f26ba5ec520e048bbbcfe90432df5d0d90bce6c
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2021-11-18 13:44:12 -0500

    C#

---
+++ Boolean Logic Assignment Submission/Boolean Logic Assignment Submission/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic_Assignment_Submission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 15, speed = 3;
+            bool DUI = true;
+            Console.WriteLine(" A basic approval program for car insurance.");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            DUI = Convert.ToBoolean(Console.ReadLine());
+         
+            Console.WriteLine("How many speeding tickets do you have?");
+            speed = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && !DUI && speed <= 3);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
