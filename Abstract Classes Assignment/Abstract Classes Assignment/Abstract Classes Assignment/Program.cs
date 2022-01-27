ce887bed81037d2adb348deab338fa8381ab1a82
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-01-27 18:23:42 -0500

    C#

---
+++ Abstract Classes Assignment/Abstract Classes Assignment/Abstract Classes Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.firstName = "Kalaipriya";
+            employee1.lastName = "Kanagaratnam";
+            employee1.sayName();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Abstract Classes Assignment/Ps/Ps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ps
+{
+    internal class Pss
+    {
+        public abstract class Ps
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public abstract void sayName();
+        }
+    }
+}
