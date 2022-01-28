5ad49f6c79ba8530f5261582a3c108e7a6506d28
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-01-27 19:04:42 -0500

    C#

---
+++ Datetime Assignment/Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            Console.WriteLine("Please enter a number that you want to enter: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Time " + userNum + " exact hours from now is: " + dateTime.AddHours(userNum));
+            Console.ReadLine();
+        }
+    }
+}
