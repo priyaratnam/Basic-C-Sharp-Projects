37d43cc08f1ff1454bfeb98ca9f45ddf8735c84e
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-01-27 20:20:01 -0500

    C#

---
+++ File IO Assignment/File IO Assignment/File IO Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give you a any number. Any number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            using (Stream_Writer file = new Stream_Writer(@"C:\Users\" true))
+            {
+                file.WriteLine(userNum);
+            }
+            string text = File.ReadAllText;
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
