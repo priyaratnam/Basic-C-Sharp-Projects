60975008d84ee82c53af1f878c81b8bea72dc6b0
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-02-01 20:52:52 -0500

    C#

---
+++ Method Assignment/Method Assignment/Ep.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    public class Operator
+    {
+        public double GetAdd(int data)
+        {
+            return data + 2;
+        }
+        public double GetSubtract(double data)
+        {
+            return data - 2;
+        }
+        public double GetDivide(double data)
+        {
+            return data / 5;
+        }
+    }
+}
--- Method Assignment/Method Assignment/Program.cs
+++ Method Assignment/Method Assignment/Program.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace Method_Assignment
 {
-    internal class Program
+    class Program
     {
+        public static int Data { get; set; }
         static void Main(string[] args)
         {
-            Employee a = new Employee() { Id = 1, name = "Movie1" };
-            Employee b = new Employee() { Id = 1, name = "Movie2" };
-            Console.WriteLine(a.Id == b.Id);
+            Console.WriteLine("Please enter a number which I will do some Math operations on.");
+            string line = Console.ReadLine();
+            Data = Int32.Parse(line);
+            var operatorObject = new Operator();
+            var result = operatorObject.GetAdd(Data);
+            Console.WriteLine("\n" + Data + " + 2 = " + result);
+            Console.ReadLine();
+            var operatorObject2 = new Operator();
+            var result2 = operatorObject2.GetSubtract(result);
+            Console.WriteLine(result + " - 2 = " + result2);
+            Console.ReadLine();
+            var operatorObject3 = new Operator();
+            var result3 = operatorObject3.GetDivide(result2);
+            Console.WriteLine(result2 + " / 5 = " + result3);
+            Console.ReadLine();
         }
     }
 }
