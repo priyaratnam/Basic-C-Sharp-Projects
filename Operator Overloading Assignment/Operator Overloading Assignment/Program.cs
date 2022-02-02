7b1f44c29a04de3eda9172ec520a2d9b4c0b4c28
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-02-01 20:59:15 -0500

    C#

---
+++ Operator Overloading Assignment/Operator Overloading Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee();
+            example.FirstName = "Priya";
+            example.LastName = "Ratnam";
+            example.ID = 00000000;
+            Employee example2 = new Employee();
+            example2.FirstName = "Mala";
+            example2.LastName = "Ratnam";
+            example2.ID = 11111111;
+            Console.WriteLine(example == example2);
+            Console.ReadLine();
+        }
+    }
+}
