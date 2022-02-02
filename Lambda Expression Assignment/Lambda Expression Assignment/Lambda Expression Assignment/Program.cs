68fe3456827cfeabdb7c0f9b08b2dd1e71a67ade
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-02-01 20:47:19 -0500

    C#

--- Lambda Expression Assignment/Lambda Expression Assignment/Lambda Expression Assignment/Program.cs
+++ Lambda Expression Assignment/Lambda Expression Assignment/Lambda Expression Assignment/Program.cs
@@ -6,37 +6,51 @@ using System.Threading.Tasks;
 
 namespace Lambda_Expression_Assignment
 {
-    internal class Program
+    class Program
     {
-        private static object thePriya;
-
         static void Main(string[] args)
         {
-            List<Employees> employees = new List<Employees>();
-            employees.Add(new Employees { FName = "Priya", LName = "thePriya", Id = 8974 });
-            employees.Add(new Employees { FName = "Priya", LName = "Ratnam", Id = 8956 });
-            employees.Add(new Employees { FName = "Mathu", LName = "Ratnam", Id = 7896 });
-            employees.Add(new Employees { FName = "Chelvi", LName = "Aran", Id = 4563 });
-            employees.Add(new Employees { FName = "Abishan", LName = "Aran", Id = 7894 });
-            employees.Add(new Employees { FName = "Trishana", LName = "Aran", Id = 1258 });
-            employees.Add(new Employees { FName = "Abishna", LName = "Aran", Id = 3645 });
-            employees.Add(new Employees { FName = "Thuva", LName = "Moorthy", Id = 4856 });
-            employees.Add(new Employees { FName = "Karan", LName = "Moorthy", Id = 4589 });
-            employees.Add(new Employees { FName = "Rangan", LName = "Ratnam", Id = 1547 });
-            employees.Add(new Employees { FName = "Ratnam", LName = "Thampi", Id = 6358 });
-            foreach (Employees employee in employees)
+            List<Employee> Employees = new List<Employee>();
+            List<string> firstnames = new List<string>()
             {
-                if (employee.FName == "Priya")
-                    thePriya.Add(employee);
+                "Priya", "Ratnam", "Mala", "Karan", "Thuva", "Abishna", "Abishan", "Trishana", "Aran", "Chelvi"
+            };
+            List<string> lastnames = new List<string>()
+            {
+                "Ratnam", "Ratnam", "Ratnam", "Ratnam", "Ratnam", "Ratnam", "Ratnam", "Ratnam", "Ratnam", "Ratnam"
+            };
+            List<int> IDs = new List<int>()
+            {
+                5, 8, 9, 10, 254, 145, 877, 658, 235, 546
+            };
+            List<int> indexes = new List<int>()
+            {
+               0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+            foreach (int index in indexes)
+            {
+                Employee Employeeobject = new Employee();
+                Employeeobject.firstname = firstnames[index];
+                Employeeobject.lastname = lastnames[index];
+                Employeeobject.ID = IDs[index];
+                Employees.Add(Employeeobject);
             }
-            List<Employees> thePriyaFNameLambda = employees.Where(x => x.FName.Contains("Priya")).ToList();
-            List<Employees> thePriyaId = employees.Where(x => x.Id > 25).ToList();
-        }
-        class Employees
-        {
-            public string FName { get; set; }
-            public string LName { get; set; }
-            public int Id { get; set; }
+            foreach (Employee Employee in Employees)
+            {
+                Console.WriteLine(firstnames[1] + " " + lastnames[1] + " " + IDs[1]);
+
+                Console.WriteLine(firstnames[4] + " " + lastnames[4] + " " + IDs[4]);
+                break;
+            }
+            foreach (Employee employee in Employees.FindAll(i => i.firstname == "Priya"))
+            {
+                Console.WriteLine(employee.firstname + " " + employee.lastname + " " + employee.ID);
+            }
+            foreach (Employee employee in Employees.FindAll(a => a.ID >= 5))
+            {
+                Console.WriteLine(employee.firstname + " " + employee.lastname + " " + employee.ID);
+            }
+            Console.ReadLine();
         }
     }
 }
