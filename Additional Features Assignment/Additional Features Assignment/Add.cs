5af7c476ed6fae5ebccb396d7783e246ced7c157
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-02-01 20:11:41 -0500

    C#

---
+++ Additional Features Assignment/Additional Features Assignment/Add.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    public class mySampleClass
+    {
+        public mySampleClass()
+        {
+            Console.WriteLine("10");
+        }
+        public mySampleClass(int x) : this()
+        {
+            Console.WriteLine("Priya");
+        }
+        public mySampleClass(int x, string y) : this(50)
+        {
+            Console.WriteLine("9");
+        }
+    }
+}
