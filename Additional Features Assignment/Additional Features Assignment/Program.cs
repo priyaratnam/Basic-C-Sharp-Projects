5eed747bf9e8df30b5558f6e420ed485fd5b1ee5
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2022-02-04 16:32:49 -0500

    Fix

--- Additional Features Assignment/Additional Features Assignment/Program.cs
+++ Additional Features Assignment/Additional Features Assignment/Program.cs
@@ -10,8 +10,33 @@ namespace Additional_Features_Assignment
     {
         static void Main(string[] args)
         {
-            mySampleClass c = new mySampleClass(50);
-            Console.Read();
+            const string COUNTRY = "Sri Lanka";
+            Console.WriteLine($"Hello!! Welcome to {COUNTRY}\n Can you plaese answer the following thr questions to help us process your entry, Thank you:");
+            Console.WriteLine("Are you travelling from inside the Canada?");
+            var EU = Console.ReadLine().ToLower();
+            if (EU == "yes" || EU == "yeah" || EU == "yup" || EU == "y")
+            {
+                Console.WriteLine("Can you please enter your first name: ");
+                var fName = Console.ReadLine();
+                Console.WriteLine("Can you please enter your last name: ");
+                var lName = Console.ReadLine();
+                Console.WriteLine($"How much money are you brining into {COUNTRY} in dollars:");
+                decimal Money = Convert.ToDecimal(Console.ReadLine());
+                Add traveller = new Add(fName, lName, Money);
+                Console.WriteLine($"Thank you so much {traveller.fName} {traveller.lName} you are entering {COUNTRY} with {traveller.Money} dollars.\n" +
+                    $"Please enjoy your visit.");
+            }
+            else if (EU == "no" || EU == "nope" || EU == "na" || EU == "n")
+            {
+                Console.WriteLine("Can you please enter your first name: ");
+                var fName = Console.ReadLine();
+                Console.WriteLine("Can you please enter your last name: ");
+                var lName = Console.ReadLine();
+                Add traveller = new Add(fName, lName);
+                Console.WriteLine($"Thank you {traveller.fName} {traveller.lName} you are clear to enter ${COUNTRY}.\n" +
+                    $"Please enjoy your visit this site.");
+            }
+            else Console.WriteLine("Please try again.");
         }
     }
 }
--- Operator Overloading Assignment/Operator Overloading Assignment/Program.cs
+++ Operator Overloading Assignment/Operator Overloading Assignment/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OperatorOverloadAssignment
+namespace Operator_Overload_Assignment
 {
     class Program
     {
